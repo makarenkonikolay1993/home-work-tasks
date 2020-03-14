8c231f80a6557033aaaf610547d186510dc7b533
makarenkonikolay1993 <61554877+makarenkonikolay1993@users.noreply.github.com>
2020-03-14 23:08:36 +0200

    HomeWork Exercise 4

--- src/test/java/com/hillel/lecture_4/SinTableTestTask.java
+++ src/test/java/com/hillel/lecture_4/SinTableTestTask.java
@@ -1,13 +1,16 @@
 package com.hillel.lecture_4;
 
+import java.text.DecimalFormat;
+
 /**
  * Написать программу, которая печатает табличку значений sin(x), где x это каждые 10 градусов от 0 до 360
  */
 public class SinTableTestTask {
 
     public static void sin() {
+        DecimalFormat sinusUgol = new DecimalFormat("##.####");
         for (int i = 0; i <= 360; i += 10) {
-            System.out.println("Синус угла " + i + " равен: " + (Math.sin(Math.toRadians(i))));
+            System.out.println("Синус угла " + i + " равен: " + sinusUgol.format(Math.sin(Math.toRadians(i))));
         }
     }
 
